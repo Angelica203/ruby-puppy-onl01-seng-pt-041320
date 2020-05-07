61395a4d5713864d06dbc30a437cf9d4a4f116c3
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-05-07 02:28:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,4 +5,29 @@ class Dog
   def intialize (name)
     @name = name
     self.save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+end
+
+  def save
+    @@all << self
+  end
+
+  def name
+    @name
+  end
+
+
 end
